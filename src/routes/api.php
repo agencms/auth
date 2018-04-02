0a7a11de118bcfa82dd547eac9ea4dbf83af2b58
Marco Mark <marco@silvanite.com>
2018-04-02 21:40:51 +0100

    Correct routing to enable password resets

--- src/routes/api.php
+++ src/routes/api.php
@@ -8,14 +8,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('agencms-auth')
     ->namespace('Agencms\Auth\Controllers')
-    ->middleware(['api', 'cors'])
+    ->middleware(['api', 'cors', 'auth:api'])
     ->group(function () {
         Route::resource('permissions', 'PermissionController');
         Route::resource('roles', 'RoleController');
         Route::resource('users', 'UserController');
         Route::resource('profile', 'ProfileController');
         Route::resource('policies', 'PolicyController');
+    });
+
+Route::prefix('agencms-auth')
+    ->namespace('Agencms\Auth\Controllers')
+    ->middleware(['cors'])
+    ->group(function () {
         Route::post('login', 'LoginController@login');
+        Route::post('password/reset', 'PasswordController@reset')->name('password.reset');
         Route::get('authorize', [
             'as' => 'login',
             'uses' => 'LoginController@required'
